d4a13ebc850e2045f2aac3d121ab14ba858fb3a9
wetey <joodotey@gmail.com>
2024-10-02 19:00:50 -0400

    move all components to welcome page

--- Welcome.py
+++ Welcome.py
@@ -1,6 +1,16 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
+import os
+
+#TODO put these all in the same object
+language = None
+embedding_type = None
+operation = None
+explain_path = None
+explain = None
+cluster = None
+cluster_path = None
 
 language_options = {
     'ar': 'arabic',
@@ -13,26 +23,12 @@ embedding_options = {
     'lf':'linguistic features',
     'concat':'concatenated'
 }
-# def create_session_states(language, embedding_type, operation):
 
-#     if 'language_selection' not in st.session_state:
-#         if language == "arabic":
-#             st.session_state['language_selection'] = 'ar'
-#         else:
-#             st.session_state['language_selection'] = 'en'
+operation_options = {
+    'labeling':'labeling',
+    'summaries': 'summaries'
+}
 
-#     if 'embedding_selection' not in st.session_state:
-#         if embedding_type == "last hidden state":
-#             st.session_state['embedding_selection'] = "lhs"
-#         elif embedding_type == "sentence-bert":
-#             st.session_state['embedding_selection'] = "sb"
-#         elif embedding_type == "linguistic features":
-#             st.session_state['embedding_selection'] = "lf"
-#         elif embedding_type == "concatenated":
-#             st.session_state['embedding_selection'] = "concat"
-
-#     if 'operation_selection' not in st.session_state:
-#         st.session_state['operation_selection'] = operation
 
 st.set_page_config(page_title="clustering and explaining errors")
 
@@ -42,7 +38,7 @@ st.markdown(
     """
     a typical NLP classification task consists of fine-tuning a language model on a specific dataset. 
     
-    these models rarely, if ever, achieve 100% accuracy. We developed a framework to help uncover the potential reasons behind the misclassification. 
+    these models rarely, if ever, achieve 100% accuracy. we developed a framework to help uncover the potential reasons behind the misclassification. 
 
     the framework consists of two stages: 1. clustering 2. generating a natural language explanation
 
@@ -50,18 +46,76 @@ st.markdown(
     """
 )
 
+with st.form("options_form", border = False):
 
-language = st.selectbox(label = "language", 
-                        options = ('ar', 'en'),
-                        format_func=lambda x: language_options.get(x),
-                        key = "language_selection")
+    #select boxes
+    language = st.selectbox(label = "language", 
+                            options = ('ar', 'en'),
+                            format_func=lambda x: language_options.get(x))
+                            # key = "language_selection")
 
-embedding_type = st.selectbox(label = "embedding type",
-                              options = ('lhs', 'sb', 'lf', 'concat'),
-                              format_func=lambda x: embedding_options.get(x),
-                              key = "embedding_selection")
+    embedding_type = st.selectbox(label = "embedding type",
+                                options = ('lhs', 'sb', 'lf', 'concat'),
+                                format_func=lambda x: embedding_options.get(x))
+                                #   key = "embedding_selection")
 
-operation = st.selectbox("Operation", 
-                    ("labeling", "summary"),
-                    key = "operation_selection")
+    operation = st.selectbox("Operation", 
+                        ("labeling", "summaries"),
+                        format_func=lambda x: operation_options.get(x))
+                        # key = "operation_selection")
+
+    submit = st.form_submit_button("submit")
+
+    if submit:
+        st.write('generating clustering...')
+
+        cluster_path = f'data/clustering/{language}_{embedding_type}_clustering.json'
+
+#TODO keep after submitting the other form
+if cluster_path:
+
+    cluster = pd.read_json(cluster_path)
+    scatter = alt.Chart(cluster).mark_point(size=300, filled=True).encode(
+                    x = alt.X('x:Q', axis=None),
+                    y = alt.Y('y:Q', axis=None),
+                    color = alt.Color('cluster:N',
+                                    scale=alt.Scale(scheme='category20b')
+                                    ).legend(direction = 'vertical', 
+                                            symbolSize = 200, 
+                                            labelFontSize=20, 
+                                            titleFontSize=24,
+                                            titleColor='#888da7',
+                                            labelColor='#888da7'),
+                    tooltip = ['cluster:N',"string_pred:N", "string_label:N"],
+                    ).properties(
+                        width = 1000,
+                        height = 600
+                    ).interactive()
+    st.altair_chart(scatter)
+
+
+explain_path = f'data/{operation}/{language}_{embedding_type}_{operation}.json'
+
+if os.path.isfile(explain_path):
+    explain = pd.read_json(explain_path)
+else:
+    st.write(f'''
+            the file you picked does not exists.
+            
+            reading the labels generated for {language_options[language]} and {embedding_options[embedding_type]}
+
+            ''')
+    explain = pd.read_json(f'data/labeling/{language}_{embedding_type}_labeling.json')
+
+with st.form("cluster", border = False):
+
+    num_clusters = tuple(explain.cluster.unique())
+    clusters = st.selectbox(label = "cluster",
+                            options = num_clusters)
+
+    submit = st.form_submit_button("submit")
+
+    #TODO fix formatting
+    if submit:    
+        st.write(explain['summary'].tolist()[clusters])
 
